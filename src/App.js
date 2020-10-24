f990645198237c7bf4b787ef557604830f6bc172
harish h r <harishbhat298@gmail.com>
2020-10-24 22:23:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layouts/Navbar.js';
+import User from './components/user/User.js';
+import Alert from './components/layouts/Alert';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+const App = () => {
+  // async componentDidMount() {
+  //   this.setState({ loading: true});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ users: res.data, loading: false});
+  // }
+
+    return (
+      <GithubState>
+        <AlertState>
+      <Router>
+                <Navbar title="Github Finder"/>
+               <div className="container">
+                      <Alert />
+                      <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/about' component={About} />  
+                        <Route exact path='/user/:login' component={User} />
+                        <Route component={NotFound}/>
+                        </Switch>
+               </div>
+      </Router>
+      </AlertState>
+      </GithubState>
+    );
+  
 }
 
 export default App;
